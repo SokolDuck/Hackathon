e37059600a746991899876ae24ce032b8e836b00
Alexander Sokolovskiy 650502 <ninjaduckyduck@gmail.com>
2019-04-03 20:17:18 +0300

    refresh models

--- main/views.py
+++ main/views.py
@@ -3,10 +3,16 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from django.views import View
 
+from main.models import Car
+
 
 class IndexView(View):
     def get(self, request, **kwargs):
-        return render(request, 'mainPage/index.html', context={})
+        if request.user is None:
+            if request.user.is_authenticated:
+                cars = Car.objects.filter(user=request.user).all()
+                return render(request, 'mainPage/index.html', context={'login': True, 'cars': cars})
+        return render(request, 'mainPage/index.html', context={'login': False})
 
     def post(self, request, **kwargs):
         return redirect(
