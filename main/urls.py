64b583dfb63ba93f3cc56e6eca4bd0a9778b4b9e
Alexander Sokolovskiy 650502 <ninjaduckyduck@gmail.com>
2019-04-03 19:54:36 +0300

    refresh models

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
+from main import views
+
 urlpatterns = [
-    path('auth/', include('authentication.urls'))
+    path('', views.IndexView.as_view(), name='index'),
 ]
