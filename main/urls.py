bc12fe5e7d8f7b6ec805bc47aa26467c8cea1f76
Vladislav Pliska <vladislavpliska@mail.ru>
2019-04-03 20:35:54 +0300

    some more views and templates

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from main import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('car/<int:pk>', views.CarView.as_view(), name='car')
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
 # Create your views here.
 from django.views import View
+from .models import Car, Note
+from django.views.generic import CreateView
 
 from main.models import Car
 
@@ -20,3 +23,26 @@ class IndexView(View):
             'shop_info',
             shop_id=request.POST.get("select")
         )
+
+
+class CarView(View):
+    def get(self, request, id):
+        object = Car.objects.filter(id=id).first()
+
+        return render(request, 'journal/jornal.html', {'records': object})
+
+
+class CarCreateView(CreateView):
+    model = Car
+    fields = ['name', 'manufacturer', 'model', 'issue_year', 'cost', 'mileage', 'registration_number',
+              'fuel_type']
+    template_name = 'create_car.html'
+
+    def form_valid(self, form):
+        self.object = Car(user_id=self.kwargs['user'], **form.cleaned_data)
+        self.object.save()
+
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse_lazy('car', args=[self.object.user_id])
